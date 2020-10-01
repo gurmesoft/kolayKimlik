4fdb4f3a6a50395bcc6bf0f4ed1c7d6535378f48
Fikret-Cin <cinfikret@gmail.com>
2020-10-02 02:51:08 +0300

    Ayar sayfası güncellendi

--- includes/settings-tcinput.php
+++ includes/settings-tcinput.php
@@ -26,31 +26,38 @@ class tcinputSettings
             __('<h1>TC INPUT</h1>', 'wookargo'),
             array($this, 'defaultSectionCallBack'),
             'tc-input-settings'
+        );  
+            add_settings_field(
+            'header',
+            __('Etki Alanları<hr>', 'wookargo'),
+            array($this,'headerCb'),
+            'tc-input-settings',
+            'defaultSection'
         );
-        add_settings_field(
+            add_settings_field(
             'woocommerce',
-            __('WooCommerce Fatura Bağlantısı', 'wookargo'),
+            __('WooCommerce Ödeme Sayfası', 'wookargo'),
             array($this, 'woocommerceCallBack'),
             'tc-input-settings',
             'defaultSection'
         );
         add_settings_field(
             'contactForm7',
-            __('Contact Form 7 Bağlantısı', 'wookargo'),
+            __('Contact Form 7', 'wookargo'),
             array($this, 'contactForm7CallBack'),
             'tc-input-settings',
             'defaultSection'
         );
         add_settings_field(
             'ninjaForm',
-            __('Ninja Form Bağlantısı', 'wookargo'),
+            __('Ninja Form', 'wookargo'),
             array($this, 'ninjaFormCallBack'),
             'tc-input-settings',
             'defaultSection'
         );
         add_settings_field(
             'wpForms',
-            __('WPForms Bağlantısı', 'wookargo'),
+            __('WPForms', 'wookargo'),
             array($this, 'wpFormCallBack'),
             'tc-input-settings',
             'defaultSection'
@@ -58,8 +65,12 @@ class tcinputSettings
         register_setting('tcSettings', 'tcSettings', array($this, 'emptyCallBack'));
     }
     public function defaultSectionCallBack(){
-        echo '<h3><p>'.__("Bu eklenti ile sitenizde kullandığınız iletişim formu eklentilerinde girilen tc kimlik bilgilerinin doğruluğunu kontrol edebilirsiniz.","tcinput").'<h3></p>';
-        echo '<p class="description">'.__("Uyarı: Sadece yüklü olan form eklentileri için aktif edilebilir.","tcinput").'</p>';
+        echo '<h3><p>'.__("Bu eklenti ile sitenizde kullandığınız iletişim formu eklentilerine, ödeme sayfalarına TC kimlik numarası alanı ekleyebilir.Girilen bilgilerin doğruluğunu kontrol edebilirsiniz.","tcinput").'<h3></p>';
+        echo '<p class="description">'.__("","tcinput").'</p><br>';
+    }
+    public function headerCB(){
+        echo '<p>'.__("Nasıl Kullanırım ?","tcinput").'</p><hr>';
+
     }
     public function woocommerceCallBack (){
         ?>
@@ -72,15 +83,21 @@ class tcinputSettings
         <p class="description">
         <input type='checkbox' id='woocommerce' name='tcSettings[woocommerceRequired]' <?php echo ( $this->tcSettings['woocommerceRequired']=='on' ? 'checked':'' ) ?> />
         <label for='tcSettings[woocommerceRequired]'> Zorunlu Alan Olarak Ekle </label></p>
+        <p class="description">Bu özellik ile WooCommerce ödeme sayfanıza TC kimlik giriş alanı ekleyebilirsiniz."Format Kontrol" girilen numaranın sadece TC kimlik numara algoritması ile kontrolünü sağlar.<br>
+        "NVI Kontrol" seçildiğinde TC kimlik ve Doğum Yılı bölümü eklenecektir. Ad,Soyad,Doğum Yılı ve TC Kimlik alanlarına girilen bilgiler nufus müdürlüğü sistemine kayıtlı bilgler ile doğrulanır.<br>
+        Her iki kontrolü de "Zorunlu Alan Olarak Ekle" seçeneği ile isteğe bağlı bırakabilir yada zorunlu kılabilirsiniz. </p>
+        <p>
+        <p> <img src="<?php echo plugin_dir_url(__DIR__)?>assets/woocommerce.png"></p>
+        <hr>
         
     <?php
     } 
     public function contactForm7CallBack (){
         ?>
-        <p class="description">Bu özellik aktif edildiğinde contact form eklentinize tckimlik,tckimlik nvi etiketleri eklenecektir.Bu etiketler ile formlarınıza tc kimlik sorgulama alanları ekleyebilirsiniz.<br>
-        Nvi opsiyonlu etiket seçildiğinde 4 adet giriş bölümü açılacaktır. Ad,Soyad,Doğum Yılı bu alanlara girilen bilgiler nufus müdürlüğü sistemine kayıtlı bilgler ile doğrulanır.<br>
+        <p class="description">Bu özellik ile contact form eklentinize tckimlik,tckimlik nvi etiketleri eklenecektir.Bu etiketler ile formlarınıza tc kimlik sorgulama alanları ekleyebilirsiniz.<br>
+        Nvi opsiyonlu etiket seçildiğinde 4 adet giriş bölümü açılacaktır. Ad,Soyad,Doğum Yılı ve TC Kimlik bu alanlara girilen bilgiler nufus müdürlüğü sistemine kayıtlı bilgler ile doğrulanır.<br>
         Her iki etiketide zorunlu kılma seçeneği formu düzenlediğiniz bölümde size sunulmuştur.</p>
-        <p> <img src="<?php echo plugin_dir_url(__DIR__)?>assets/contactform7.png"></p>
+        <p> <img src="<?php echo plugin_dir_url(__DIR__)?>assets/contactform7.png"></p><hr>
         <?php
     }
     public function ninjaFormCallBack (){
