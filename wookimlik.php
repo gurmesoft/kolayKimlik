264bfbf6149e8d361b95fa9d889e85df4d5f27fd
Yunus ALTIN <yunusaltin1963@gmail.com>
2022-03-03 08:11:34 +0300

    autoload

--- wookimlik.php
+++ wookimlik.php
@@ -19,17 +19,40 @@ require_once 'includes/settings-wookimlik.php';
 require_once 'includes/class-contactform7.php';
 require_once 'includes/class-ninjaform.php';
 require_once 'includes/class-woocommerce-checkout.php';
+require __DIR__ . '/vendor/autoload.php';
 
+/**
+ * Initialize the plugin tracker
+ *
+ * @return void
+ */
+function appsero_init_tracker_tc_kimlik_vergi_no_dogrulama_wookimlik()
+{
+
+    if (!class_exists('Appsero\Client')) {
+        require_once __DIR__ . '/appsero/src/Client.php';
+    }
+
+    $client = new Appsero\Client('1a6f9ca6-536a-450b-877a-f0adadb5d297', 'T.C Kimlik & Vergi No Dogrulama - wooKimlik', __FILE__);
+
+    // Active insights
+    $client->insights()->init();
+
+    // Active automatic updater
+    $client->updater();
+}
+
+appsero_init_tracker_tc_kimlik_vergi_no_dogrulama_wookimlik();
 
 register_activation_hook(__FILE__, 'tcSettingsActiveHook');
-register_deactivation_hook( __FILE__, 'tcSettingsDeactiveHook' );
+register_deactivation_hook(__FILE__, 'tcSettingsDeactiveHook');
 
 
 $SettingsPage = new tcinputSettings();
 $wooCheckOut = new wooCheckOut();
 
-if(class_exists("WPCF7")){
-    $CTF7 = new contactFormSeven();  
+if (class_exists("WPCF7")) {
+    $CTF7 = new contactFormSeven();
 }
 // if(class_exists("WooCommerce")){
 //         $wooCheckOut = new wooCheckOut();  
