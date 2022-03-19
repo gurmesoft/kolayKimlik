e600c5d8243c05a39c09c323dd323f88acbf656e
Yunus ALTIN <yunusaltin1963@gmail.com>
2022-03-19 15:30:29 +0300

    config

--- includes/class-ninjaform.php
+++ includes/class-ninjaform.php
@@ -1,42 +1,4 @@
 <?php
-<<<<<<< HEAD
-require_once ('functions-kontrol.php');
-class ninjaForm
-{   
-    public function __construct(){
-
-        add_filter( 'ninja_forms_submit_data', array($this,'bilgiKontrolNinjaForm'), 10, 2 );
-    }
-
-    public function bilgiKontrolNinjaForm( $form_data ) {
-
-        foreach( $form_data[ 'fields' ] as $field ) { 
-   
-            if( 'tc-no' == $field[ 'key' ] ) {
-                $tc = $field[ 'value' ];
-                if(! standartSorgulama($tc)) {
-    
-                    $errors = [
-                        'fields' => [
-                          'tc-no' => __( 'Hatalı Tc No Formatı', 'tcinput' ),
-                        ]
-                      ];
-                    
-                    $response = [
-                        'errors' => $errors,
-                    ];
-                    
-                    echo wp_json_encode( $response );
-                }              
-
-            }          
-             
-        } 
-
-        return $form_data;
-    } 
-}    
-=======
 require_once('functions-kontrol.php');
 class WK_ninjaForm
 {
@@ -73,4 +35,3 @@ class WK_ninjaForm
         return $form_data;
     }
 }
->>>>>>> main
