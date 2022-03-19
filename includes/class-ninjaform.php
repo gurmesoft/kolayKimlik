8cd30e540583262f72fb04aa71f973a45e19a006
Yunus ALTIN <yunusaltin1963@gmail.com>
2022-03-19 15:06:33 +0300

    sonar

--- includes/class-ninjaform.php
+++ includes/class-ninjaform.php
@@ -1,37 +1,37 @@
 <?php
-require_once ('functions-kontrol.php');
+require_once('functions-kontrol.php');
 class WK_ninjaForm
-{   
-    public function __construct(){
+{
+    public function __construct()
+    {
 
-        add_filter( 'ninja_forms_submit_data', array($this,'bilgiKontrolNinjaForm'), 10, 2 );
+        add_filter('ninja_forms_submit_data', array($this, 'bilgiKontrolNinjaForm'), 10, 2);
     }
 
-    public function bilgiKontrolNinjaForm( $form_data ) {
+    public function bilgiKontrolNinjaForm($form_data)
+    {
 
-        foreach( $form_data[ 'fields' ] as $field ) { 
-   
-            if( 'tc-no' == $field[ 'key' ] ) {
-                $tc = $field[ 'value' ];
-                if(! standartSorgulama($tc)) {
-    
+        foreach ($form_data['fields'] as $field) {
+
+            if ('tc-no' == $field['key']) {
+                $tc = $field['value'];
+                if (!standartSorgulama($tc)) {
+
                     $errors = [
                         'fields' => [
-                          'tc-no' => __( 'Hatalı Tc No Formatı', 'tcinput' ),
+                            'tc-no' => __('Hatalı Tc No Formatı', 'tcinput'),
                         ]
-                      ];
-                    
+                    ];
+
                     $response = [
                         'errors' => $errors,
                     ];
-                    
-                    echo wp_json_encode( $response );
-                }              
 
-            }          
-             
-        } 
+                    echo wp_json_encode($response);
+                }
+            }
+        }
 
         return $form_data;
-    } 
-}    
+    }
+}
