39172ccfcf569791959fc8376f4f8167e8685495
Fuat Poyraz <fuat@gurmesoft.com>
2022-04-13 14:16:09 +0300

    appsero apisi güncellendi

--- kolaykimlik.php
+++ kolaykimlik.php
@@ -3,7 +3,7 @@
  * Plugin Name: kolaykimlik
  * Plugin URI: https://www.gurmewoo.com/
  * Description: T.C kimlik onayı ve Vergi Numarası onayı ile kullanıcı kayıtları, ödeme işlemleri ve çeşitli form eklentileri (Contact Form, NinjaForms, WPForms) kullanarak özelleştirilmiş formlar oluşturmaları için geliştirilmiştir.
- * Version: 1.3.5
+ * Version: 1.3.6
  * Author: GurmeWoo.com
  * Author URI: https://www.gurmewoo.com
  * Plugin URI: https://gurmewoo.com/product/kolaykimlik-wordpress-woocommerce-kimlik-ve-vergi-numarasi-kontrol-eklentisi
@@ -28,9 +28,7 @@ function kk_appsero_init_tracker_tc_kimlik_vergi_no_dogrulama_kolay_kimlik_dorul
 	if ( ! class_exists( 'Appsero\Client' ) ) {
 		require_once __DIR__ . '/appsero/src/Client.php';
 	}
-
-	$client = new Appsero\Client( '413cb71d-9871-47e4-9c85-b1ca59681eb9', 'T.C Kimlik & Vergi No Dogrulama - Kolay Kimlik Doğrulama', __FILE__ );
-
+	$client = new Appsero\Client( '5a5d672c-ffef-47fa-a920-eeb38b9627b2', 'T.C Kimlik & Vergi No Dogrulama & Kolay Kimlik Doğrulama', __FILE__ );
 	// Active insights
 	$client->insights()->init();
 
